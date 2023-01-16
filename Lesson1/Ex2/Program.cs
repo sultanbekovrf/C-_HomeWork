d92f8605852119da7648d9c3a86f888d932b385e
Rinat_Sultanbekov <sultanbekovrf@mail.ru>
2023-01-16 23:04:42 +0500

    Создана структура файла README.md

---
+++ Lesson1/Ex2/Program.cs
@@ -0,0 +1,5 @@
+﻿// На вход принимает два числа и выдаёт, какое число большее, а какое меньшее.сде
+Console.WriteLine("Введите число A: ");
+int numberA = Console.ReadLine();
+Console.WriteLine("Введите число B: ");
+int numberB = 7;
