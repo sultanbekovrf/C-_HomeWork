8d7083ded9ea9b3b4146da44e5dc3a6de8f6f4f6
Rinat_Sultanbekov <sultanbekovrf@mail.ru>
2023-01-18 18:39:31 +0500

    Решили задачу 4

---
+++ Lesson1/Ex4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Принимает на вход три числа и выдает максимальное
+Console.WriteLine("Введите число A ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("max = " + max);
