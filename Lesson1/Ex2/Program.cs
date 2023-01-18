d56fba240a16d426034bc56510abc17d84f6668e
Rinat_Sultanbekov <sultanbekovrf@mail.ru>
2023-01-18 17:50:15 +0500

    Решили задачу 2

--- Lesson1/Ex2/Program.cs
+++ Lesson1/Ex2/Program.cs
@@ -1,5 +1,16 @@
-﻿// На вход принимает два числа и выдаёт, какое число большее, а какое меньшее.сде
-Console.WriteLine("Введите число A: ");
-int numberA = Console.ReadLine();
-Console.WriteLine("Введите число B: ");
-int numberB = 7;
+﻿// На вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число A ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Первое число " + numberA + " больше, чем второе число " + numberB);
+}
+
+else
+{
+    Console.WriteLine("Второе число " + numberB + " больше, чем первое число " + numberA);
+}
