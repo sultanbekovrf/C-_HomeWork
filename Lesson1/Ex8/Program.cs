c4ee269e0a66f324c77d86376ba8951733d528bf
Rinat_Sultanbekov <sultanbekovrf@mail.ru>
2023-01-26 22:37:29 +0500

    Решили задачу №15, добавили ссылки на файлы

--- Lesson1/Ex8/Program.cs
+++ Lesson1/Ex8/Program.cs
@@ -1,13 +1,10 @@
 ﻿// на вход число N, на выходе показать все четные числа от 1 до N
 Console.WriteLine("Введите натуральное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-int i = 1;
+int i = 2;
 Console.WriteLine("Четные числа от 1 до " + a + ":");
 while (i <= a)
 {
-   if (i % 2 == 0)
-   {
-          Console.Write(i + " ");
-   }
-   i++;
+   Console.Write(i + " ");
+   i = i + 2;
 }
