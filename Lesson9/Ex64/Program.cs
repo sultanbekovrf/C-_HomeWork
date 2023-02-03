7d6da5beece4494f0fe479e13cd6c7afa0e851a5
Rinat_Sultanbekov <sultanbekovrf@mail.ru>
2023-02-03 15:37:52 +0500

    решили задачу 64

---
+++ Lesson9/Ex64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии
+
+void PrintNumbers(int N)
+{
+    if(N==1)
+    {
+        Console.Write(N);
+        return;
+    }
+    {
+        Console.Write($"{N}, ");
+        PrintNumbers(N-1);
+    }
+}
+
+
+Console.Write("Введите натуральное число: ");
+int N = int.Parse(Console.ReadLine());
+
+PrintNumbers(N);
